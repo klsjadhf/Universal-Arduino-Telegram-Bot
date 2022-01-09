b1f924bf54fd6f95ff9ddccd76f02081d1439198
klsjadhf <>
2022-01-10 01:42:12 +0800

    Add location sending

---
+++ examples/ESP32/SendLocation/SendLocation.ino
@@ -0,0 +1,67 @@
+/*******************************************************************
+    A telegram bot for your ESP32 that sends 
+    a location every 30 seconds
+ *******************************************************************/
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+
+// Wifi network station credentials
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+// Telegram BOT Token (Get from Botfather)
+#define BOT_TOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+// Use @myidbot (IDBot) to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "123456789"
+
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  // attempt to connect to Wifi network:
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+}
+
+void loop()
+{
+  //coordinates for testing purposes replace with data from gps
+  float latitude = 1.3484278835359604;
+  float longitude = 103.68313469847416;
+
+  Serial.print("Latitude:");
+  Serial.print(latitude);
+  Serial.print(" Longitude:");
+  Serial.println(longitude);
+  bot.sendLocation(CHAT_ID, latitude, longitude);
+  
+  delay(30000);
+}
